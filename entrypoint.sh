93713fa663854b46a63390d52d605cb2fc99763c
lucasuds <101261158+lucasuds@users.noreply.github.com>
2022-04-08 12:44:02 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 git_setup() {
   cat <<- EOF > $HOME/.netrc
-		machine github.com
-		login $GITHUB_ACTOR
-		password $GITHUB_TOKEN
-		machine api.github.com
-		login $GITHUB_ACTOR
-		password $GITHUB_TOKEN
+    machine github.com
+    login $GITHUB_ACTOR
+    password $GITHUB_TOKEN
+    machine api.github.com
+    login $GITHUB_ACTOR
+    password $GITHUB_TOKEN
 EOF
   chmod 600 $HOME/.netrc
 
@@ -23,7 +23,8 @@ git_cmd() {
   fi
 }
 
-PR_BRANCH="auto-$INPUT_PR_BRANCH-$GITHUB_SHA"
+BRANCH_NEW="cherry-pick/auto-$GITHUB_SHA"
+
 MESSAGE=$(git log -1 $GITHUB_SHA | grep "AUTO" | wc -l)
 
 if [[ $MESSAGE -gt 0 ]]; then
@@ -36,7 +37,7 @@ PR_TITLE=$(git log -1 --format="%s" $GITHUB_SHA)
 git_setup
 git_cmd git remote update
 git_cmd git fetch --all
-git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
+git_cmd git checkout -b "${BRANCH_NEW}" origin/develop
 git_cmd git cherry-pick "${GITHUB_SHA}"
-git_cmd git push -u origin "${PR_BRANCH}"
-git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
+git_cmd git push -u origin "${BRANCH_NEW}"
+git_cmd hub pull-request -b "develop" -h "${BRANCH_NEW}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
